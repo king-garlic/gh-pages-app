187f40f096c47984968af41b24d0e06da974a6df
sangbinlee <sangbinlee9@gmail.com>
2022-05-15 00:38:07 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/CT3KEC2OafX7SSMNKOJn2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-06b9b971e804acd5.js"],"/_error":["static/chunks/pages/_error-98e801c646abf33c.js"],"/about":["static/chunks/pages/about-3ada7c2c9e76ff10.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
